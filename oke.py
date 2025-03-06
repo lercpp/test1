950d74050c6ec34691b934aa711d122c3dcd272c
lercpp <flashiap08@gmail.com>
2025-03-06 18:25:23 +0300

    okeoke

--- oke.py
+++ oke.py
@@ -18,3 +18,9 @@ def filter_equal(arr, value):
         if arr[i]==value:
             a.append(arr[i])
     return a
+def filter_not(arr, value):
+    a=[]
+    for i in range(len(arr)):
+        if arr[i]!=value:
+            a.append(arr[i])
+    return a
