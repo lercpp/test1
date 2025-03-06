a83b6ae36cc400198d4d4192cbe60a5eba9dd88b
lercpp <flashiap08@gmail.com>
2025-03-06 18:21:17 +0300

    okeoke

--- oke.py
+++ oke.py
@@ -4,3 +4,10 @@ def find_greater(arr, value):
         if arr[i]>=value:
             a.append(arr[i])
     return a
+
+def filter_less(arr, value):
+    a=[]
+    for i in range(len(oke)):
+        if arr[i]<value:
+            a.append(arr[i])
+    return a
